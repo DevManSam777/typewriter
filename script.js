95c765401bcce7b6728e85109863af0d00ed2143
Sam S <devmansam@protonmail.com>
2025-02-21 03:16:20 -0800

    loop

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // Javascript Typewriter Effect
+const textDisplay = document.getElementById("text");
+let currentPhrase = [];
+let i = 0;
+let j = 0;
+let isDeleting = false;
+
+const phrases = ["Hello World!", "Enjoy the typewriter effect.", "Take care."];
+
+function loop() {
+    textDisplay.innerHTML = phrases[0];
+    console.log('print');
+    
+
+    if (i < phrases.length) {
+        if (!isDeleting && j <= phrases[i].length) {
+            currentPhrase.push(phrases[i][j]);
+            j++;
+            console.log(currentPhrase);
+            
+        } 
+    }
+
+    if (j == phrases[i].length) {
+        i++;
+        j = 0;
+    }
+   
+    const time = 2000;
+    setTimeout(loop, time) 
+}
+
+// loop();
